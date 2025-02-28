b3dc50199407486954c5081733f95d8706656986
nandhinibs1999 <nandhinibalasundar1@gmail.com>
2025-02-28 16:55:52 +0530

    Resolved merge conflicts

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tests;
 
 import org.testng.Assert;
@@ -51,57 +50,3 @@ public class SearchTest extends BaseTest {
         Assert.assertFalse(results.isEmpty(), "❌ No search results found for keyword: " + keyword);
     }
 }
-=======
-package tests;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.annotations.DataProvider;
-import pages.HomePage;
-import pages.SearchResultsPage;
-import utils.BaseTest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.List;
-
-public class SearchTest extends BaseTest {
-    private static final Logger logger = LogManager.getLogger(SearchTest.class);
-
-    @DataProvider(name = "searchKeywords")
-    public Object[][] getKeywords() {
-        return new Object[][] {
-            {"mobile"},
-            {"python"},
-            {"job"}
-        };
-    }
-
-    @Test(dataProvider = "searchKeywords")
-    public void testSearchFunctionality(String keyword) throws InterruptedException {
-        logger.info("=== Starting search test for keyword: " + keyword + " ===");
-
-        HomePage homePage = new HomePage(driver);
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-
-        // Perform search
-        homePage.search(keyword);
-        logger.info("Search performed for: " + keyword);
-
-        // Capture search results (fetch top 2 results)
-        List<String> results = searchResultsPage.getTopSearchResults(2); 
-
-        if (results.isEmpty()) {
-            logger.error("❌ No search results found for keyword: " + keyword);
-        } else {
-            logger.info("✅ First Search Result: " + results.get(0));
-            if (results.size() > 1) {
-                logger.info("✅ Second Search Result: " + results.get(1));
-            }
-        }
-
-        // Assert that at least one result is found
-        Assert.assertFalse(results.isEmpty(), "❌ No search results found for keyword: " + keyword);
-    }
-}
->>>>>>> 9175ea99b0a12c8642595105e823ba480af5f8de
--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -49,55 +49,4 @@ public class BaseTest {
         }
     }
 }
-=======
-package utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.By;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-
-import java.time.Duration;
-
-public class BaseTest {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-
-    @BeforeMethod
-    @Parameters("browser")
-    public void setup(@Optional("chrome") String browser) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        } else {
-            throw new IllegalArgumentException("Invalid browser name: " + browser);
-        }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        driver.get("https://www.aventusinformatics.com/");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();  // This will close all tabs and quit the driver instance
-        }
-    }
-}
->>>>>>> 9175ea99b0a12c8642595105e823ba480af5f8de
