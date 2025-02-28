b74d223f9da35411789457f23a32b3111e2df168
Nandhini Balasundar <98599302+nandhinibs1999@users.noreply.github.com>
2025-02-28 16:27:00 +0530

    Add files via upload

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -3,11 +3,16 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 import java.time.Duration;
 
 public class BaseTest {
@@ -15,26 +20,31 @@ public class BaseTest {
     protected WebDriverWait wait;
 
     @BeforeMethod
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Invalid browser name: " + browser);
+        }
 
-        // Maximize window and set timeouts
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
-        // Navigate to website
         driver.get("https://www.aventusinformatics.com/");
-
-        // Wait until page loads
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
     }
 
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-            driver.quit();
+            driver.quit();  // This will close all tabs and quit the driver instance
         }
     }
 }
